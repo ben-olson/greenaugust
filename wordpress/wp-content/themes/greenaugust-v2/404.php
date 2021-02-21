aa21b12851a1afdd50f57badaacdd15afba64430
Benjamin Olson <benjaminolson@Benjamins-MacBook-Pro.local>
2021-02-20 20:14:34 -0800

    Added Assignment 5

--- wordpress/wp-content/themes/greenaugust-v2/404.php
+++ wordpress/wp-content/themes/greenaugust-v2/404.php
@@ -1,9 +1,11 @@
 <?php get_header(); ?>
 <?php get_template_part('templates/template', 'header'); ?>
 
-<div class="heading msg-404">
+<div class="[ upper font:light font-size:s4 msg-404 ]">
   Looks like the page you were looking for doesn't exist! Sorry about that.
 </div>
+
+<div class="[ pagination ]">&nbsp;</div>
 
 <?php get_template_part('templates/template', 'footer'); ?>
 <?php get_footer(); ?>
